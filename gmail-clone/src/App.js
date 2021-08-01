53c8a253f922be11cef9e67f5f5fb9fb3a44b2f0
szilard-molnar <mszilard86@gmail.com>
2021-08-01 15:51:02 -0400

    email list design created

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Mail from './Mail';
+import EmailList from './EmailList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="app">
       <Header />
-      <Sidebar />
+      <div className="app__body">
+        <Sidebar />
+
+        <Switch>
+          <Route path="/mail">
+            <Mail />
+          </Route>
+          <Route path="/">
+            <EmailList />
+          </Route>
+        </Switch>
+      </div>
+      
     </div>
+    </Router>
   );
 }
 
---
+++ gmail-clone/src/Mail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Mail.css';
+
+function Mail() {
+    return (
+        <div className="mail">
+            <h3>Mail...</h3>
+        </div>
+    )
+}
+
+export default Mail
