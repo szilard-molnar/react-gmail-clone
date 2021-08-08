9493e07f987e3dfbd92efef84583e44a271dbd26
szilard-molnar <mszilard86@gmail.com>
2021-08-08 14:59:43 -0400

    google authentication

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -7,13 +7,29 @@ import EmailList from './EmailList';
 import SendMail from './SendMail';
 import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
-import { selectUser } from './features/userSlice';
+import { selectUser, login } from './features/userSlice';
+import { auth } from './firebase';
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if(user) {
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL
+        }))
+      } else {
+        
+      }
+    })
+  }, []);
 
   return (
     <Router>
--- gmail-clone/src/Login.js
+++ gmail-clone/src/Login.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Login.css';
+import { auth, provider } from './firebase';
+import Button from '@material-ui/core/Button';
+import { login } from './features/userSlice';
+import { useDispatch } from 'react-redux';
 
 function Login() {
+    const dispatch = useDispatch();
+
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+            .then(({ user }) => {
+                dispatch(login({
+                    displayName: user.displayName,
+                    email: user.email,
+                    photoUrl: user.photoURL,
+                }))
+            })
+            .catch(error => alert(error.message));
+    }
+
     return (
         <div className="login">
             <div className="login__container">
                 <img src="https://static.dezeen.com/uploads/2020/10/gmail-google-logo-rebrand-workspace-design_dezeen_2364_col_0.jpg" alt="gmail logo" />
+
+                <Button variant="contained" color="primary" onClick={signIn}>Login</Button>
             </div>
         </div>
     )
