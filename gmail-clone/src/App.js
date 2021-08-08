95e5a172819c4163f391a33afb637fa7b1d7fae3
szilard-molnar <mszilard86@gmail.com>
2021-08-08 14:01:48 -0400

    login page created

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -5,32 +5,39 @@ import Sidebar from './Sidebar';
 import Mail from './Mail';
 import EmailList from './EmailList';
 import SendMail from './SendMail';
+import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
+import { selectUser } from './features/userSlice';
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
 
   return (
     <Router>
-    <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
 
-        <Switch>
-          <Route path="/mail">
-            <Mail />
-          </Route>
-          <Route path="/">
-            <EmailList />
-          </Route>
-        </Switch>
-      </div>
-      
-      {sendMessageIsOpen && <SendMail />}
-    </div>
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+          
+          {sendMessageIsOpen && <SendMail />}
+        </div>
+      )}
     </Router>
   );
 }
