4a15aa8c569fe10143331494a39f8d5af3d76438
szilard-molnar <mszilard86@gmail.com>
2021-08-08 15:16:49 -0400

    optional chaining debugging

--- gmail-clone/src/Header.js
+++ gmail-clone/src/Header.js
@@ -6,11 +6,19 @@ import { Avatar, IconButton } from '@material-ui/core';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectUser, logout } from './features/userSlice';
+import { auth } from './firebase';
 
 function Header() {
     const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
+    const signOut = () => {
+        auth.signOut().then(() => {
+            dispatch(logout())
+        })
+    }
 
     return (
         <div className="header">
@@ -34,7 +42,7 @@ function Header() {
                 <IconButton>
                     <NotificationsIcon />
                 </IconButton>
-                <Avatar src={user?.photoUrl}/>
+                <Avatar src={user?.photoUrl} onClick={signOut} className="header__avatar"/>
             </div>
         </div>
     )
